6126bc900af18f86e43addf2a4d4acda721ab60b
MantakSingh <44886815+MantakSingh@users.noreply.github.com>
2026-01-15 21:42:30 -0800

    Create binarycounter.cpp

---
+++ binarycounter.cpp
@@ -0,0 +1,29 @@
+int redPin = 12;
+int bluePin = 8;
+int greenPin = 5;
+int yellowPin = 3;
+
+void setup() {
+
+pinMode(redPin,OUTPUT);
+pinMode(bluePin,OUTPUT);
+pinMode(greenPin,OUTPUT);
+pinMode(yellowPin,OUTPUT);
+
+}
+
+void loop() {
+
+// Red
+digitalWrite(redPin,HIGH);
+
+// Blue
+digitalWrite(bluePin,HIGH);
+
+// Green
+digitalWrite(greenPin,HIGH);
+
+// Yellow
+digitalWrite(yellowPin,HIGH);
+
+}
