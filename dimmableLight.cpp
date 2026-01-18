4f6fc4f13a6cdcf8201322168e62d1f49946646b
MantakSingh <44886815+MantakSingh@users.noreply.github.com>
2026-01-18 00:48:21 -0800

    Create dimmableLight.cpp

---
+++ dimmableLight.cpp
@@ -0,0 +1,16 @@
+int potPin = A1;
+int gPin = 6;
+int potVal;
+float LEDVal;
+void setup() {
+  pinMode(potPin,INPUT);
+  pinMode(gPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  potVal = analogRead(potPin);
+  LEDVal = (255./1023.)*potVal;
+  analogWrite(gPin,LEDVal);
+  Serial.println(LEDVal);
+}
