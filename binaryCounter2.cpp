8aa4d98ff57af78140a30dc26e26aa4869c7af10
MantakSingh <44886815+MantakSingh@users.noreply.github.com>
2026-01-22 17:50:21 -0800

    Update binaryCounter2.cpp

--- binaryCounter2.cpp
+++ binaryCounter2.cpp
@@ -1,8 +1,7 @@
 int latchPin=11;
 int clockPin=9;
 int dataPin=12;
-int dt=1000;
-byte LEDs;
+int dt=250;
 
 void setup() {
   Serial.begin(9600);
@@ -12,14 +11,12 @@ void setup() {
 }
 
 void loop() {
-  for(int i = 0b00000000; i < 255; i++){
-    LEDs = 
+  for(int i = 0; i < 255; i++){
+    i = byte(i);
     digitalWrite(latchPin,LOW);
     shiftOut(dataPin,clockPin,LSBFIRST,i);
     digitalWrite(latchPin,HIGH);
     Serial.println(i);
     delay(dt);
-
   }
-
 }
