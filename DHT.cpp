d46f74bbe1c373906f8a2a128d8eca50a7fbc8a0
MantakSingh <44886815+MantakSingh@users.noreply.github.com>
2026-01-25 17:49:39 -0800

    Create DHT.cpp

---
+++ DHT.cpp
@@ -0,0 +1,32 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define Type DHT11
+int sensePin=2;
+DHT HT(sensePin,Type);
+float humidity;
+float tempC;
+float tempF;
+int setTime=500;
+int dt=1000;
+
+void setup() {
+  Serial.begin(9600);
+  HT.begin();
+  delay(setTime);
+}
+
+void loop() {
+  humidity=HT.readHumidity();
+  tempC=HT.readTemperature();
+  tempF=HT.readTemperature(true);
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("% Temperature ");
+  Serial.print(tempC);
+  Serial.print(" C ");
+  Serial.print(tempF);
+  Serial.println(" F ");
+  delay(dt);
+}
